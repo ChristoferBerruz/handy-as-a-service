866ee25b7169416576b7c505de392af6f90a7d2d
Christofer Berruz Chungata <cberruzc@my.bridgeport.edu>
2021-04-21 13:58:38 -0400

    Actual implementation of Handy network

---
+++ handy_concrete.py
@@ -0,0 +1,13 @@
+from handy_core import IHandyNetwork
+
+"""
+This is the network that should be implemented
+"""
+class HandyConcrete(IHandyNetwork):
+    pass
+
+
+
+
+# This module should have the object initialized
+handy = HandyConcrete()
